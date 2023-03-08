c69b0f3aad76543114e8c940c8ff652e3bdd8e96
mcsiple-NOAA <margaret.siple@noaa.gov>
2023-03-08 16:08:29 +0100

    initial commit - different effort or prob bycatch by area

---
+++ spatial_simplification.R
@@ -0,0 +1,15 @@
+# Generate fishing effort
+# both the
+pbycatch_area <- function(nareas, evenness_alpha){
+  
+}
+
+# Mean number of events per boat OR total number of boats could be different by area
+effort_by_area <- function(nareas, 
+                           effort_unit = "mean.fishing.event.boat", 
+                           evenness_alpha){
+  areas_vec <- 1:nareas
+  if(effort_unit == "mean.fishing.event.boat"){
+    #proportion_
+  }
+}
